fb3e7e542be577f03f0b775401958078b987b608
Ilaria <128404113+ilariaciavarella@users.noreply.github.com>
2023-04-16 11:49:40 +0200

    Complete the exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let strArr = str.split("");
+    let reverseArr = [];
+    for (let i = 0; i < strArr.length; i++) {
+        reverseArr.unshift(strArr[i]);
+    }
+    return reverseArr.join("");
 };
 
 // Do not edit below this line
