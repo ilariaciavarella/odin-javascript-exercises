b9d7bfba547889e607287bd526edd96f7bf57e1b
Ilaria <128404113+ilariaciavarella@users.noreply.github.com>
2023-04-18 18:35:27 +0200

    Complete the exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(addends) {
+  let sum = 0;
+	for (addend of addends) {
+    sum += addend;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(factors) {
+  let mult = 1;
+	for (factor of factors) {
+    mult *= factor;
+  }
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let fact = 1;
+  if (a >= 1) {
+    for (let i = 1; i <= a; i++) {
+      fact *= i;
+    }
+  }
+  return fact;
 };
 
 // Do not edit below this line
