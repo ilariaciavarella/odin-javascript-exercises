0892c8a9b1ce82bbc6920d203107a9a5e0d985fe
Ilaria <128404113+ilariaciavarella@users.noreply.github.com>
2023-04-18 22:59:35 +0200

    Complete exercise (with help)

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    //Filter non-letters
+    let toFil = str.toLowerCase().split('');
+
+    let filtered = toFil.filter(char => letters.includes(char))
+
+    let newStr = filtered.join("");
+    console.log(newStr);
+
+    return newStr === filtered.reverse().join("");
 
 };
+
+console.log(palindromes('ZZZZ car, a man, a maracaz.'))
+
 
 // Do not edit below this line
 module.exports = palindromes;
