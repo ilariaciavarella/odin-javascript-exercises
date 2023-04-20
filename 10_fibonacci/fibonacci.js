8adbe96c91e1adc01fd1e2231c72fbe33f9de070
Ilaria <128404113+ilariaciavarella@users.noreply.github.com>
2023-04-20 17:02:34 +0200

    Complete the exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(pos) {
+    let fib = [1, 1];
+    
+    for (let i = 2; i < pos; i++) {
+        fib.push(fib[i-2] + fib [i-1]);
+    }
+
+    if (pos <= 0) {
+        return 'OOPS';
+    } else {
+        return fib[pos-1];
+    }
+
 
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
