b34da0eb1610064e3a1cc7d4eabc2f83a99ed23a
Ilaria <128404113+ilariaciavarella@users.noreply.github.com>
2023-04-21 18:01:46 +0200

    Complete the exercise but different from solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,35 +1,17 @@
 const today = new Date().getFullYear();
 console.log(today);
 
-const people = [
-      {
-        name: "Carly",
-        yearOfBirth: 1942,
-        yearOfDeath: 1970,
-      },
-      {
-        name: "Ray",
-        yearOfBirth: 1962,
-        yearOfDeath: 2011,
-      },
-      {
-        name: "Jane",
-        yearOfBirth: 1912,
-        //yearOfDeath: 1941,
-      },
-    ]
-
 const findTheOldest = function(objArr) {
-  let mapped = objArr.map(person => {
+  objArr.map(person => {
     if (person.hasOwnProperty('yearOfDeath')) {
-      return person.yearOfDeath - person.yearOfBirth;
+      return person.age = person.yearOfDeath - person.yearOfBirth;
     } else {
-      return today - person.yearOfBirth;
+      return person.age = today - person.yearOfBirth;
     }});
-  return mapped;
-};
 
-console.log(findTheOldest(people));
+  objArr.sort((a, b) => a.age < b.age ? 1 : -1);
+  return objArr[0];
+};
 
 // Do not edit below this line
 module.exports = findTheOldest;
